0d388387e8b641c9793dcc1ceeeb3026297f5ac0
robert.jantyik <robert.jantyik@dextraline.hu>
2022-08-31 12:29:40 +0200

    init

---
+++ src/main/java/hu/asami/asamilogistics/AsamiLogisticsApplication.java
@@ -0,0 +1,13 @@
+package hu.asami.asamilogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsamiLogisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsamiLogisticsApplication.class, args);
+    }
+
+}
